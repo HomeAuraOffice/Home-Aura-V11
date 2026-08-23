c1237aa1fdd4a674943a104717d247bac4fe01de
HomeAuraOffice <officehomeaura@gmail.com>
2026-08-23 22:56:21 +0600

    feat: enhance clipboard parsing engine
    Improve phone number and name extraction logic by adding Bengali numeral support and cleaner regex patterns. Remove obsolete test files.

--- test_parse2.js
+++
@@ -1,9 +0,0 @@
-const formatBangladeshDisplayTime = (isoOrDate) => {
-  const d = new Date(isoOrDate);
-  return d.toLocaleString('en-GB', {
-     timeZone: 'Asia/Dhaka',
-     day: '2-digit', month: 'short', year: 'numeric',
-     hour: '2-digit', minute: '2-digit', hour12: true
-  });
-};
-console.log(formatBangladeshDisplayTime("2026-08-21 21:30"));
